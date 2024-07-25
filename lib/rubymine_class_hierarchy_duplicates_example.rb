4d476467132c82784b489c7dc24dee0f7f2fa615
Mark McDonald <mark.mcdonald@greenhouse.io>
2024-07-25 10:42:24 -0500

    Initial commit

---
+++ lib/rubymine_class_hierarchy_duplicates_example.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rubymine_class_hierarchy_duplicates_example/version"
+
+module RubymineClassHierarchyDuplicatesExample
+  class Error < StandardError; end
+  # Your code goes here...
+end
